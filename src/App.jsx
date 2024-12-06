85467978d07883c826bc4539ab6b801b091c3dc4
Nick <NickolasKavanagh@proton.me>
2024-12-06 14:38:28 -0330

    scrollability fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ import Counter from './components/Counter';
 
 function App() {
   return (
-    <div className="relative min-h-screen -z-10 w-full overflow-y-auto bg-[#f0f0f0]">
+    <div className="relative min-h-screen w-full overflow-y-auto bg-[#f0f0f0]">
       <motion.div
-        className="absolute inset-0 min-h-screen w-full bg-[linear-gradient(to_right,#ee151580_1px,transparent_1px),linear-gradient(to_bottom,#ee151580_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"
+        className="fixed inset-0 min-h-screen w-full bg-[linear-gradient(to_right,#ee151580_1px,transparent_1px),linear-gradient(to_bottom,#ee151580_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)] pointer-events-none z-0"
         animate={{
           x: [14, 0],
         }}
@@ -20,7 +20,7 @@ function App() {
           ease: "linear",
         }}
       />
-      <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
+      <main className="relative z-1 flex flex-col items-center px-4 md:px-8 lg:px-16">
         <Navbar />
         <Hero />
         <Dex />
