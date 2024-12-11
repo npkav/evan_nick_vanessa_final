8bbf01ce2202f5360e06f63afef39e562ddd49c8
Nick <NickolasKavanagh@proton.me>
2024-12-11 13:20:12 -0330

    updated counter styling to mirror dex

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -12,25 +12,26 @@ const Counter = () => {
   };
 
   return (
-    <div className="flex flex-col items-center bg-gray-100 p-4 rounded-md shadow-md w-64">
-      <h2 className="text-2xl font-bold mb-4 text-pokeball">PokéCounter</h2>
-      <p className="text-3xl font-semibold text-pokeball mb-4">{count}</p>
-      <div className="flex gap-4 mb-4">
+    <div className="w-full max-w-4xl p-6 my-8 bg-[#f0f0f0] rounded-lg border-2 border-[#222224] relative">
+      <h2 className="text-3xl font-bold mb-4">PokéCounter</h2>
+      <p className="text-4xl font-semibold mb-6">{count}</p>
+      <div className="flex gap-4 mb-6">
         <button
           onClick={() => setCount(Math.max(0, count - increment))}
-          className="px-4 py-2 bg-pokeball text-white rounded-md hover:bg-pokeball-hover"
+          disabled={count === 0}
+          className="px-4 py-2 bg-[#ee1515] text-white rounded-lg disabled:opacity-0 w-[120px] opacity-40 hover:opacity-100 transition-all duration-300 active:scale-95 transform"
         >
           -
         </button>
         <button
           onClick={() => setCount(count + increment)}
-          className="px-4 py-2 bg-pokeball text-white rounded-md hover:bg-pokeball-hover"
+          className="px-4 py-2 bg-[#ee1515] text-white rounded-lg disabled:opacity-0 w-[120px] opacity-40 hover:opacity-100 transition-all duration-300 active:scale-95 transform"
         >
           +
         </button>
       </div>
-      <div className="flex items-center mb-4">
-        <label htmlFor="increment" className="text-sm mr-2">
+      <div className="flex items-center mb-6">
+        <label htmlFor="increment" className="text-lg mr-4">
           Increment:
         </label>
         <input
@@ -39,7 +40,7 @@ const Counter = () => {
           min="1"
           value={increment}
           onChange={handleIncrementChange}
-          className="w-16 p-1 border border-gray-300 rounded-md text-center"
+          className="w-24 p-2 border rounded-lg text-center"
         />
       </div>
       <button
@@ -47,7 +48,7 @@ const Counter = () => {
           setCount(0);
           setIncrement(1);
         }}
-        className="px-6 py-2 bg-pokeball text-white rounded-md hover:bg-pokeball-hover"
+        className="px-4 py-2 bg-[#ee1515] text-white rounded-lg disabled:opacity-0 w-[120px] opacity-40 hover:opacity-100 transition-all duration-300 active:scale-95 transform"
       >
         Reset
       </button>
